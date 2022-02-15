ec9061479cea5166986d9148fd8405ea03e4e1e7
jademisato <87796782+jademisato@users.noreply.github.com>
2022-02-15 12:00:41 +0900

    feat: add a route
    add routing and page links

--- nearbnb/src/App.js
+++ nearbnb/src/App.js
@@ -3,31 +3,36 @@ import './App.css';
 import Title from './Title';
 import POPOSList from './POPOSList';
 import Footer from './Footer';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './About'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+    <Router>
       <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header> */}
+
         <Title />
-        <POPOSList />
-        <Footer />
+          <Routes>
+          <Route exact path="/" element={<POPOSList />} />
+          <Route path="/about" element={<About />} />
+          </Routes>
+          <Footer />
       </div>
-    </div>
+    </Router>
   );
 }
 
--- nearbnb/src/Title.js
+++ nearbnb/src/Title.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import './Title.css';
+import { NavLink } from 'react-router-dom'
 
 function Title() {
   return (
     <div className="Title">
-      <h1>SFPOPOS</h1>
-      <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+      <header>
+        <h1>SFPOPOS</h1>
+        <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+
+        <div>
+          <NavLink
+            className="nav-link"
+            exact
+            to="/">List</NavLink>
+
+          <NavLink
+            className="nav-link"
+            to="/about">About</NavLink>
+        </div>
+      </header>
     </div>
   )
 }
