233c1414f7857d4f5877c10bec1ff2b76c094188
jademisato <87796782+jademisato@users.noreply.github.com>
2022-02-22 21:00:10 +0900

    refactor : grouping by file type
    add a components folder and moved related files under it

--- nearbnb/src/components/POPOSDetails.js
+++ nearbnb/src/components/POPOSDetails.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import data from './sfpopos-data.json';
+import data from '../sfpopos-data.json';
 import { useParams } from "react-router-dom"
 
 function POPOSDetails(props) {
