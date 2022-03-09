1dd1b8cc818592a37053d3e91ad94223c8cab5e5
jademisato <87796782+jademisato@users.noreply.github.com>
2022-03-09 19:18:21 +0900

    feat: connect to firebase
    add firebase connection information

--- nearbnb/src/components/App.js
+++ nearbnb/src/components/App.js
@@ -7,7 +7,42 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './About'
 import POPOSDetails from './POPOSDetails'
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { initializeApp } from 'firebase/app';
+
+
+// TODO: ↓cut out firebase connection information to firebase.js later ------------------
+// import firebase from "firebase";
+import "firebase/firestore"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8RtusJ6znoD77R8dGCx6WZizN8ONoJiY",
+  authDomain: "nearbnb-31341.firebaseapp.com",
+  projectId: "nearbnb-31341",
+  storageBucket: "nearbnb-31341.appspot.com",
+  messagingSenderId: "626902569388",
+  appId: "1:626902569388:web:84a1353cd14f6544366a09",
+  measurementId: "G-9QQK6LL30M"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const app = initializeApp(firebaseConfig);
+// firebase.initializeApp(config);
+var db = firebase.firestore();
+
+// export default firebase
+
+// TODO: END ----------------------------------------------------------
+
 function App() {
+
+  const ref = firebase.firestore().collection("places")
+  console.log(ref);
+  
   return (
     <Router>
       <div className="App">
--- nearbnb/src/firebase.js
+++ nearbnb/src/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase";
+import "firebase/firestore"
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA8RtusJ6znoD77R8dGCx6WZizN8ONoJiY",
@@ -16,6 +12,10 @@ const firebaseConfig = {
   measurementId: "G-9QQK6LL30M"
 };
 
-// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// firebase.initializeApp(config);
+var db = firebase.firestore();
+
+export default firebase
