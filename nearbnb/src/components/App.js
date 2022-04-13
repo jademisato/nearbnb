e51f8f3c801268ec9884cb212e17c8f4fc98333c
jademisato <87796782+jademisato@users.noreply.github.com>
2022-04-13 09:53:18 +0900

    wip: connect to storage
    to use gsutil for solve CORS error

--- nearbnb/src/components/App.js
+++ nearbnb/src/components/App.js
@@ -6,8 +6,46 @@ import Footer from './Footer';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './About'
 import POPOSDetails from './POPOSDetails'
+import { storage } from "../firebase"
+import { getStorage, ref, getDownloadURL } from "firebase/storage"
 
 function App() {
+
+  // TODO: ↓ Fix connnect to cloud storage late(Installing gsutil again for solve CORS "https://cloud.google.com/storage/docs/gsutil_install", i didn't commit google-cloud-sdk because over 100MB.)
+  const storage = getStorage();
+  const spaceRef = ref(storage, 'test/shika.jpeg');
+
+  // nullRef is null, since the parent of root is null
+  const nullRef = spaceRef.root.parent;
+
+  console.log("↓spaceRef");
+  console.log(spaceRef);
+  console.log("↑spaceRef");
+
+  //Download
+  getDownloadURL(spaceRef)
+    .then((url) => {
+      console.log("↓url");
+      console.log(url);
+      console.log("↑url");
+      const xhr = new XMLHttpRequest();
+      console.log("↓xhr");
+      console.log(xhr);
+      console.log("↑xhr");
+      xhr.responseType = 'blob';
+      // xhr.responseType = set("Access-Control-Allow-Origin", "*");
+      xhr.onload = (event) => {
+        const blob = xhr.response;
+      };
+      xhr.open('GET', url);
+      xhr.send();
+      console.log(url);
+
+      const img = document.getElementById('myimg');
+      img.setAttribute('src', url);
+    });
+  // TODO: ↑ Fix connnect to cloud storage later
+
   return (
     <Router>
       <div className="App">
@@ -25,6 +63,12 @@ function App() {
             Learn React
           </a>
         </header> */}
+
+        <div>
+          {/* <img src="https://i.ytimg.com/vi/CFZcxLMUSYI/maxresdefault.jpg" /> */}
+          <img src={spaceRef} />
+
+        </div>
 
         <Title />
           <Routes>
