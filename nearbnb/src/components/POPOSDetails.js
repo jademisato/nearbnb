225d5da04cf7e853e97ff722b21983b3b912a693
jademisato <87796782+jademisato@users.noreply.github.com>
2022-05-19 13:11:09 +0900

    style: add style to Details Page
    place the information on the left and the image on the right and divide the space in 1/3 and 2/3 proportion.

--- nearbnb/src/components/POPOSDetails.js
+++ nearbnb/src/components/POPOSDetails.js
@@ -2,6 +2,7 @@ import db from "../firebase"
 import { doc, getDoc } from "firebase/firestore";
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
+import './POPOSDetails.css'
 
 function POPOSDetails(props) {
   let params = useParams()
@@ -23,16 +24,16 @@ function POPOSDetails(props) {
   }, [])
   
   return (
-    <div>
-      <div>
+    <div className="POPOSDetails">
+      <div className="POPOSDetails-image">
         <img src={place.imgURL} />
         {/* <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} /> */}
       </div>
-      <div>
-        <h1>{place.title}</h1>
-        <p>{place.desc}</p>
-        <p>{place.hours}</p>
-        <p>{place.features}</p>
+      <div className="POPOSDetails-info">
+        <h1 className="POPOSDetails-title">{place.title}</h1>
+        <p className="POPOSDetails-desc">{place.desc}</p>
+        <p className="POPOSDetails-hours">{place.hours}</p>
+        <p className="POPOSDetails-features">{place.features}</p>
         {/* <p>{geo.lat} {geo.lon}</p> */}
       </div>
     </div>
