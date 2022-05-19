af9179be61111db18d932c1a4b681b79bcbb60bf
jademisato <87796782+jademisato@users.noreply.github.com>
2022-05-19 12:27:24 +0900

    style: add style to spaces
    Information is displayed so that it overlaps the image, and the position of the characters has been changed.

--- nearbnb/src/components/POPOSSpace.js
+++ nearbnb/src/components/POPOSSpace.js
@@ -1,27 +1,26 @@
 import React from 'react'
-// import logo from './logo.svg'
 import './POPOSSpace.css';
 import { Link } from 'react-router-dom'
 
 function POPOSSpace(props) {
   const { name, image, address, hours, id } = props
-  // console.log("space")
-  // console.log(props)
-  // console.log("space")
 
   return (
-    <div>
+    <div className="POPOSSpace">
       <Link to={`/details/${id}`}>
         <img src={image} width="300" height="300" alt="image" />
       </Link>
       <h1>
-        <Link to={`/details/${id}`}>
+        <Link
+          className="POPOSSpace-title"
+          to={`/details/${id}`}>
           {name}
         </Link>
       </h1>
-      <div>{address}</div>
-      <div>{hours}</div>
-      <div>{id}</div>
+      <div className="POPOSSpace-info">
+        <div>{address}</div>
+        <div>{hours}</div>
+      </div>
     </div>
   )
 }
